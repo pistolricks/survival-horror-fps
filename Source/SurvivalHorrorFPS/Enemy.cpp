befd19183557c0a120cc399b09844c34db60fb90
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-07-30 03:03:33 -0700

    **Remove unnecessary Tick setting from Enemy class constructor**

--- Source/SurvivalHorrorFPS/Enemy.cpp
+++ Source/SurvivalHorrorFPS/Enemy.cpp
@@ -10,7 +10,6 @@
 AEnemy::AEnemy()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
 
 }
 
